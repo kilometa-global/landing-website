f1d136feef280e5a9f32c3b79849c0ab4458ff0d
LexduTech <88058375+LexduTech@users.noreply.github.com>
2023-01-02 00:26:49 +0100

    Update header.php

--- header.php
+++ header.php
@@ -31,7 +31,30 @@
 </head>
 
 <body data-spy="scroll" data-target=".navbar" data-offset="71">
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCAOphdBX2dJcwHYzgy7dB3qjYI0FjUeMs",
+    authDomain: "kilometa-global.firebaseapp.com",
+    databaseURL: "https://kilometa-global-default-rtdb.firebaseio.com",
+    projectId: "kilometa-global",
+    storageBucket: "kilometa-global.appspot.com",
+    messagingSenderId: "693476810676",
+    appId: "1:693476810676:web:9ad72bf5bee41f459bdce1",
+    measurementId: "G-KMMVFS0Y1T"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
   <!-- NAVBAR -->
   <nav class="navbar navbar-expand-xl navbar-light fixed-top py-2 border-bottom" style="background-color: #fffffff7;">
     <div class="container-fluid">
